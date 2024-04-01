6748aa20bf06507edfc988d98666907df7f5cb7e
noah <35225015+noahbaumgartner@users.noreply.github.com>
2024-04-01 14:49:15 +0200

    added score

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,19 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {   
     public float speed = 10.0f;
     public Transform cameraTransform;
     public LoopingBackground loopingBackground;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI highscoreText;
+
+    private int score = 0;
+    private int highscore = 0;
+
+    void Start()
+    {
+        scoreText.text = "Score: " + score.ToString();
+    }
 
     void FixedUpdate()
     {
         float input = Input.GetAxis("Horizontal");
         float moveSpeed = speed * Time.deltaTime;
 
-        transform.rotation *= Quaternion.Euler(0, 0, input);
+        transform.rotation *= Quaternion.Euler(0, 0, input*2);
         Vector3 moveDirection = speed * Time.deltaTime * transform.up;
         transform.position -= moveDirection;
 
@@ -26,10 +37,15 @@ public class PlayerController : MonoBehaviour
     void OnTriggerEnter2D(Collider2D trigger)
     {
         Debug.Log("Trigger entered");
-        if (trigger.CompareTag("FAIL"))
-        {
+        if (trigger.CompareTag("FAIL")) {
             Debug.Log("You lose");
             ReloadLevel();
+        }
+
+        if(trigger.CompareTag("PASS")) {
+            score++;
+            highscore++;
+            scoreText.text = "Score: " + score.ToString();
         }
     }
 
